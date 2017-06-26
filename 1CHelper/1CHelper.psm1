ff9808b6f8fa0daa5a9c16633c586e5c4b2482cf
Eugene <makey.ewgeniy@gmail.com>
2017-06-26 17:01:46 +0300

    Update 1CHelper.psm1

--- 1CHelper/1CHelper.psm1
+++ 1CHelper/1CHelper.psm1
@@ -73,8 +73,13 @@ function Get-TechJournalData
 
     Begin {
         # Данный шаблон соответствует одной записи журнала
+        <# Не захватывает свойства вида "Context=Форма.ЗаблокироватьДанныеФормыДляРедактирования : Документ.СписаниеНДС.Форма.ФормаДокумента"
         $template = @"
 ^(?<line>(?<time>\d\d\:\d\d\.\d{6}\-\d)\,(?<type>\w+)\,(?<level>\d)(\,(?<name>(\w+\:)?\w+)\=(?<value>([\w\-]+|(\"[^"]+\")|(\'[^']+\'))?))+.*)
+"@
+#>
+        $template = @"
+^(?<line>(?<time>\d\d\:\d\d\.\d{6}\-\d)\,(?<type>\w+)\,(?<level>\d)(\,(?<name>(\w+\:)?\w+)\=(?<value>([^"'\,\n]+|(\"[^"]+\")|(\'[^']+\'))?))+.*)
 "@
         $regex = New-Object System.Text.RegularExpressions.Regex ($template, [System.Text.RegularExpressions.RegexOptions]::Multiline)
         $tree = @()

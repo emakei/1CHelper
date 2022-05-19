edf7147ece446e09a7fdceb4aaffc46a24cf5e78
Yauhen Makei <yauhen.makei@gmail.com>
2022-05-19 14:48:55 +0300

    Это здесь не нужно

--- Полезные запросы/Определение фрагментации индексов.sql
+++
@@ -1,6 +0,0 @@
-
-select N'erp' batabase_name, * from sys.dm_db_index_physical_stats(DB_ID(N'erp'), NULL, NULL, NULL , 'DETAILED') where avg_fragmentation_in_percent >= 20 order by avg_fragmentation_in_percent desc
-go
-
-select N'accbe' batabase_name, * from sys.dm_db_index_physical_stats(DB_ID(N'accbe'), NULL, NULL, NULL , 'DETAILED') where avg_fragmentation_in_percent >= 20 order by avg_fragmentation_in_percent desc
-go

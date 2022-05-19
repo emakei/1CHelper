45ebb7163a6cda575407541f9831902be70bcd2f
Yauhen Makei <yauhen.makei@gmail.com>
2022-05-19 14:45:43 +0300

    Nothing

--- Полезные запросы/Определение фрагментации индексов.sql
+++ Полезные запросы/Определение фрагментации индексов.sql
@@ -1,6 +1,6 @@
 
-select N'BelGAZ' batabase_name, * from sys.dm_db_index_physical_stats(DB_ID(N'BelGAZ'), NULL, NULL, NULL , 'DETAILED') where avg_fragmentation_in_percent >= 20 order by avg_fragmentation_in_percent desc
+select N'erp' batabase_name, * from sys.dm_db_index_physical_stats(DB_ID(N'erp'), NULL, NULL, NULL , 'DETAILED') where avg_fragmentation_in_percent >= 20 order by avg_fragmentation_in_percent desc
 go
 
-select N'accbel' batabase_name, * from sys.dm_db_index_physical_stats(DB_ID(N'accbel'), NULL, NULL, NULL , 'DETAILED') where avg_fragmentation_in_percent >= 20 order by avg_fragmentation_in_percent desc
+select N'accbe' batabase_name, * from sys.dm_db_index_physical_stats(DB_ID(N'accbe'), NULL, NULL, NULL , 'DETAILED') where avg_fragmentation_in_percent >= 20 order by avg_fragmentation_in_percent desc
 go

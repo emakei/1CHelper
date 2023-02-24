33d729f6380a7c2220c3430f59e0ca9848df305c
Yauhen Makei <yauhen.makei@gmail.com>
2023-02-24 15:43:24 +0300

    Просто чтобы не потерять

---
+++ Microsoft.PowerShell_profile.ps1
@@ -0,0 +1,40 @@
+# Get-WindowsCapability -Name RSAT* -Online | Add-WindowsCapability -Online
+# Get-CimClass -Namespace root/CIMV2 | Where-Object CimClassName -Like Win32* | Select-Object CimClassName | Sort-Object -Property CimClassName
+
+Set-PSReadLineOption -EditMode Vi -ViModeIndicator Prompt
+
+## Map PSDrives to other registry hives
+if (!(Test-Path HKCR:)) {
+    $null = New-PSDrive -Name HKCR -PSProvider Registry -Root HKEY_CLASSES_ROOT
+    $null = New-PSDrive -Name HKU -PSProvider Registry -Root HKEY_USERS
+}
+
+## Customize the prompt
+function prompt {
+    $identity = [Security.Principal.WindowsIdentity]::GetCurrent()
+    $principal = [Security.Principal.WindowsPrincipal] $identity
+    $adminRole = [Security.Principal.WindowsBuiltInRole]::Administrator
+
+    $prefix = $(if (Test-Path variable:/PSDebugContext) { '[DBG]: ' }
+                elseif ($principal.IsInRole($adminRole)) { "[ADMIN]: " }
+                else { '' })
+    $body = 'PS ' + $(Get-Location)
+    $suffix = $(if ($NestedPromptLevel -ge 1) { '>>' }) + '> '
+    $prefix + $body + $suffix
+}
+
+Set-PSReadLineKeyHandler -Chord 'Ctrl+p' -Function PreviousHistory
+Set-PSReadLineKeyHandler -Chord 'Ctrl+n' -Function NextHistory
+Set-PSReadLineKeyHandler -Chord 'Ctrl+e' -Function EndOfLine
+Set-PSReadLineKeyHandler -Chord 'Ctrl+a' -Function BeginningOfLine
+Set-PSReadLineKeyHandler -Chord 'Enter' -Function ValidateAndAcceptLine
+
+## Add argument completer for the dotnet CLI tool
+$scriptblock = {
+    param($wordToComplete, $commandAst, $cursorPosition)
+    dotnet complete --position $cursorPosition $commandAst.ToString() |
+        ForEach-Object {
+            [System.Management.Automation.CompletionResult]::new($_, $_, 'ParameterValue', $_)
+        }
+}
+Register-ArgumentCompleter -Native -CommandName dotnet -ScriptBlock $scriptblock
